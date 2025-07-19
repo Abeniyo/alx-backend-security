7c307bb9b3f2fff78d6b3942480197c92cd028ef
Abeniyo <avelox6867@gmail.com>
2025-07-19 22:54:51 +0300

    New

---
+++ alx_backend_security/ip_tracking/middleware.py
@@ -0,0 +1,14 @@
+from .models import RequestLog
+from django.utils.deprecation import MiddlewareMixin
+from ipware import get_client_ip
+
+class IPLoggingMiddleware(MiddlewareMixin):
+    def process_request(self, request):
+        ip, _ = get_client_ip(request)
+        if ip is None:
+            ip = '0.0.0.0'
+
+        RequestLog.objects.create(
+            ip_address=ip,
+            path=request.path
+        )
